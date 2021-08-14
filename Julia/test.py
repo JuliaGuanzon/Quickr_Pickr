7638e12c4fb7c53b0ec4ce4bfeb797ab0ce82d06
Julia Guanzon <84649228+JuliaGuanzon@users.noreply.github.com>
2021-08-14 14:33:28 -0700

    Added timeline, and other meeting notes, updates file

--- Julia/test.py
+++ Julia/test.py
@@ -5,25 +5,34 @@ import questionary
 rsi_indicator = questionary.select(
         "Regarding relative strength index, what are you looking for?",
         choices = [
-            "I'm looking for an RSI of 70 or greater",
-            "I'm looking for an RSI between 30 and 70",
-            "I'm looking for an RSI of 30 or less"
+            "I'm looking for an RSI of 51 or greater",
+            "I'm looking for an RSI of 50 or less"
         ]
     ).ask()
 
-volume_indicator = questionary.select(
-        "Are you intersted in stocks that have high or low volume?",
-        choices = [
-            "High Volume",
-            "Low Volume"
-        ]
-    ).ask()
+if rsi_indicator == "I'm looking for an RSI of 51 or greater":
+    rsi = Todays_RSI_Hi()
 
-price_indicator = questionary.select(
-        "How much are you willing to pay?",
-        choices = [
-            "I want to spend less than $100 on one stock",
-            "I want to spend less than $1000 on one stock",
-            "I do not have a limit"
-        ]
-    ).ask()
+else:
+    print()
+
+# data["RSI"] = data["RSI"].apply(lambda x: x if x >= 51)
+# x if x <= 50
+
+
+# volume_indicator = questionary.select(
+#         "Are you intersted in stocks that have high or low volume?",
+#         choices = [
+#             "High Volume",
+#             "Low Volume"
+#         ]
+#     ).ask()
+
+# price_indicator = questionary.select(
+#         "How much are you willing to pay?",
+#         choices = [
+#             "I want to spend less than $100 on one stock",
+#             "I want to spend less than $1000 on one stock",
+#             "I do not have a limit"
+#         ]
+#     ).ask()
