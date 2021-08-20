99b7939c7e5fb54a6e758b598d008072ec9b7528
Julia Guanzon <84649228+JuliaGuanzon@users.noreply.github.com>
2021-08-20 07:11:14 -0700

    organized and updated files

--- quickr_pickr.py
+++ quickr_pickr.py
@@ -16,17 +16,13 @@ from termcolor import colored as cl
 import matplotlib.pyplot as plt
 import pprint
 
-
 plt.rcParams['figure.figsize'] = (20, 10)
 plt.style.use('fivethirtyeight')
 from pathlib import Path
 pp = pprint.PrettyPrinter(indent=4)
-# from utils.financial_functions import *
-from utils.sr_financial_functions import *
-from utils.fileio import save_csv
+from utils.financial_functions import *
 
-#J.Guanzon Comment: The questionary below will greet the user and ask if they would like to use the program.
-# If they choose yes, the program will run get_ticks which calls the S&P 500 data. If they choose no, they will be exited out the program.
+
 def introduction():
     starter_question = questionary.select(
         "Welcome! Are you ready to use Quickr Pickr?",
@@ -38,8 +34,18 @@ def introduction():
 
     if starter_question == "YES":
         return get_ticks()
+        
     else:
         sys.exit(f"Goodbye!")
+
+def long_or_short():
+    answer = questionary.select("Would you like to see short or long term indicators?", choices=[
+        "Short Term Indicators",
+        "Long Term Indicators"
+    ]).ask()
+
+    return answer
+
 
 def get_indicators(sp500_ticks):
     #J.Guanzon Comment: Next questionary will ask if the user would like to download the most recent momentum data.
@@ -67,6 +73,30 @@ def get_indicators(sp500_ticks):
     else:
         return todays_indicators(sp500_ticks)
 
+def get_long_indicators(sp500_ticks):
+    recent_long_data = questionary.select(
+        "Would you like to download our most recent long term data? This will take a while to download.",
+        choices = [
+            "YES",
+            "NO"
+        ]
+    ).ask()
+
+    if recent_long_data == "YES":
+        return get_longterm(sp500_ticks)
+    elif recent_long_data == "NO":
+        ask_again =questionary.select(
+            "Are you sure?",
+            choices= [
+                "YES",
+                "NO"
+            ]
+        ).ask()
+    if ask_again == "YES":
+        sys.exit(f"Goodbye!")
+    else:
+        return get_longterm(sp500_ticks)
+
 def choose_indicator():
     answer = questionary.select(
     "What indicator would you like to sort the S&P 500 by?",
@@ -77,13 +107,22 @@ def choose_indicator():
         "Low MACD",
         "High SMA50%",
         "Low SMA50%",
-        "Low MACD Divergence",
-        "Debt to Equity",
-        "Total Debt",
-        "Market Cap"#,
-#        "Total Revenue"
+        "Low MACD Divergence"
     ]  
     ).ask()
+
+    return answer
+
+def choose_long_indicator():
+    answer = questionary.select("What indicator would you like to sort the S&P 500 by?",
+    choices=[
+        "High Total Debt",
+        "Low Total Debt",
+        "High Debt to Equity",
+        "Low Debt to Equity",
+        "Market Cap",
+        "Forward P/E"
+    ]).ask()
 
     return answer
 
@@ -92,9 +131,14 @@ def sort_by_indicator(df, indicator):
 
     return sorted_df
 
+def sort_by_long_indicator(df, indicator):
+    sorted_long_df = sort_longterm(df, indicator)
+
+    return sorted_long_df
+
 def chart_or_info_q(ticker_list):
     answer = questionary.select(
-        "Would you like to see the chart for additional information about a particular stock?",
+        "Would you like to see the chart for or additional information about a particular stock?",
         choices=[
             "Chart",
             "Info",
@@ -103,7 +147,6 @@ def chart_or_info_q(ticker_list):
     ).ask()
     if answer == "Chart":
         ticker = questionary.text("Please input the ticker you'd like to see a chart of").ask().upper()
-        # Saeed: Adding ERROR checking
         if ticker in  ticker_list:
             chart = get_chart(ticker)
             return chart
@@ -131,20 +174,7 @@ def chart_or_info_q(ticker_list):
             sys.exit(f"Thank you for using Quickr Pickr! See you soon!")
         else:
             return chart_or_info_q(ticker_list)
-            
-def save_stock_picks(result):
-    """Saves the qualifying stocks to a CSV file.
-
-    Args:
-        qualifying_stocks (list of lists): The qualifying stocks.
-    """
-    # @TODO: Complete the usability dialog for savings the CSV Files.
-    # J.Guanzon Comment- Used questionary.confirm to prompt a "yes/no" response. By saying "y" to the prompt, the user can save the file, and the program will advise the location of the file.
-    csvpath = questionary.confirm("Would you like to save these picks?").ask()
-    csvpath = Path('./top_picks/stockpicks.csv')
-    save_csv(csvpath,result)
-    print (f"The file is located here: {csvpath}.")
-
+    
 
 def run():
 
@@ -152,28 +182,46 @@ def run():
     sp500_ticks = introduction()
     # print(sp500_ticks)
 
-    # Build the indicator DataFrame
-    test_list = ["AAPL", "WMT", "GOOG"] #SW: Test list for speed of testing, will be sp500_ticks
-    today_df = get_indicators(test_list) #SW: get_indicators will use (sp500_list) in final build
+    # User decides between short and long term
+    answer = long_or_short()
+    if answer == "Short Term Indicators":
 
-    print(today_df)
+        # Build the indicator DataFrame
+        test_list = ["AAPL", "WMT", "GOOG"] #SW: Test list for speed of testing, will be sp500_ticks
+        today_df = get_indicators(test_list) #SW: get_indicators will use (sp500_ticks) in final build
 
-    # Let user choose Indicator
-    indicator = choose_indicator()
+        print(today_df)
 
-    # Sort by chosen indicator
-    sorted_list = sort_by_indicator(today_df, indicator)
-    sorted_list_head = sorted_list.head(2) #SW: Thinking of adding an option to choose how many results you'd like.
-    print(sorted_list_head)
+        # Let user choose Indicator
+        indicator = choose_indicator()
 
+        # Sort by chosen indicator
+        sorted_list = sort_by_indicator(today_df, indicator)
+        sorted_list_head = sorted_list.head(2) #SW: Thinking of adding an option to choose how many results you'd like.
+        print(sorted_list_head)
+
+    else:
+        test_list = ["AAPL", "GOOG", "WMT"] #SW: Just a test list, this will be sp500_ticks
+        df = get_long_indicators(test_list) # SW: Will use (sp500_ticks)
+
+        print(df)
+
+        # Let user choose indicator
+        long_indicator = choose_long_indicator()
+
+        # Sort by chosen indicator
+        sorted_long = sort_by_long_indicator(df, long_indicator)
+        sorted_long_head = sorted_long.head(2)
+        print(sorted_long_head)
+
+        #
     # Check to see if the user would like a chart or more info on a specific stock.
-    result = chart_or_info_q(sp500_ticks)
+    result = chart_or_info_q(test_list) #SW: Will be sp500_ticks
     pp.pprint(result)
 
     sys.exit(f"Thank you for using Quickr Pickr! See you soon!")
 
-    # save_stock_picks(result)
+# SW: I'd like to figure out a "main loop" for iteration number 2
 
 if __name__ == "__main__":
     fire.Fire(run)
-    
